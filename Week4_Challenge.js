fad76df9722ded1301fb7d76553954c0054a0c38
christineessma <christine.essma@gmail.com>
2023-05-23 11:04:59 -0500

    coding challenges 4 and 5

--- Week4_Challenge.js
+++ Week4_Challenge.js
@@ -1,1 +1,17 @@
 
+function LongestWord(sen) { 
+  let sentenceArray = sentence.replace(/^0-9a-zA-Z\s]/g, "").split("");
+  let longWord = "";
+
+  for (let i = 0; i < sentenceArray.length; i++) {
+  if(sentenceArray[i].length > longWord.length) {
+    longWord = sentenceArray[i];}
+  }
+    // code goes here  
+    return longWord; 
+  
+  }
+     
+  // keep this function call here 
+  console.log(LongestWord("here is a sample sentence for testing."));
+
