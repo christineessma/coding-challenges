88faa9f01a22704bd5080085da38342c37f384d6
christineessma <christine.essma@gmail.com>
2023-06-13 17:22:50 -0500

    coding practice 6-8

--- Week6_Challenge.js
+++ Week6_Challenge.js
@@ -1,1 +1,12 @@
 
+function detectWord(string) {
+    let wantedWord = [];
+    for (let i = 0; i < string.length; i++) {
+        if (string[i] !== (string[i]).toUpperCase()) {
+            wantedWord.push(string[i]);
+        }
+    }
+    return wantedWord.join('');
+}
+
+console.log(detectWord("YFemHUFBbezFBYzFBYLleGBYEFGBMENTment"));
--- Week8_Challenge.js
+++ Week8_Challenge.js
@@ -1,1 +1,10 @@
+function findCharNumber (char, str) {
+    let sum = 0
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === char) {
+            sum += 1;
+        }
+    }
+    return sum;
 
+}
